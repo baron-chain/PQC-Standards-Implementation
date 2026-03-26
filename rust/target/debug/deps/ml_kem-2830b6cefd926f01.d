983d3e71c6a3cff0e7dfefc80fec7ef6f45d7a8f
Liviu Epure <liviu.etty@gmail.com>
2026-03-26 22:22:08 +0200

    chore: add .gitignore and remove tracked build artifacts

--- rust/target/debug/deps/ml_kem-2830b6cefd926f01.d
+++
@@ -1,5 +0,0 @@
-/Users/liviu/Development/GIT/PQC-Standards-Implementation/rust/target/debug/deps/ml_kem-2830b6cefd926f01.d: ml-kem/src/lib.rs
-
-/Users/liviu/Development/GIT/PQC-Standards-Implementation/rust/target/debug/deps/libml_kem-2830b6cefd926f01.rmeta: ml-kem/src/lib.rs
-
-ml-kem/src/lib.rs:
